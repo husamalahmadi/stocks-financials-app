705d7c513cc85273605f5aa4e3d3e96c2aaaaef5
husamalahmadi <husamalahmadi@gmail.com>
2026-01-15 20:10:32 +0000

    New before Deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/stocks-financials-app/docs",
+  base: "/stocks-financials-app/",
   plugins: [react()],
   build: {
     outDir: "docs",
