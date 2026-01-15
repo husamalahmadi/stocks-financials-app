78fbfb1e8bff7030604b8967654864aa975aca3f
husamalahmadi <husamalahmadi@gmail.com>
2026-01-15 21:59:22 +0000

    done with everthing

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/stocks-financials-app/",
+  base: "/", 
   plugins: [react()],
   build: {
-    outDir: "docs",
+    outDir: "output",
     emptyOutDir: true,
   },
 });
