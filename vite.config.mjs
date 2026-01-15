ccf94621d1525d88d0299e7dc665573acc7b418c
husamalahmadi <husamalahmadi@gmail.com>
2026-01-15 11:26:51 +0000

    Build for deployment (output folder)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,10 @@
-// FILE: client/vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  build: {
+    outDir: "output",
+    emptyOutDir: true,
+  },
 });
